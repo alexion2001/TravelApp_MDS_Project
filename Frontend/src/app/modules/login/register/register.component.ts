34098d9cf681f2542a21018f2575de8bd400ee7b
Alexandra Ion <alexion200123@gmail.com>
2022-05-11 17:37:18 +0300

    login/register

--- Frontend/src/app/modules/login/register/register.component.ts
+++ Frontend/src/app/modules/login/register/register.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { AbstractControl, FormControl, FormGroup } from '@angular/forms';
+
 
 @Component({
   selector: 'app-register',
@@ -7,9 +10,62 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RegisterComponent implements OnInit {
 
-  constructor() { }
+  public registerForm: FormGroup = new FormGroup( 
+    {
+        nume: new FormControl(''),
+        prenume: new  FormControl(''),
+        email: new  FormControl(''),
+        tel: new  FormControl(''),
+        parola: new  FormControl('')
+    });
 
-  ngOnInit(): void {
+
+  constructor(private router: Router,
+    ) { }
+
+    get nume(): FormGroup{
+      return this.registerForm.get('nume') as FormGroup;
+    }
+    get parola(): AbstractControl{
+      return this.registerForm;
+    }
+    get tel(): AbstractControl{
+      return this.registerForm;
+    }
+  
+    get prenume(): AbstractControl{
+      return this.registerForm;
+    }
+    get email(): AbstractControl{
+      return this.registerForm;
+    }
+  
+  
+
+  ngOnInit(): void {}
+
+    public register(): void {
+      
+      console.log("Te-ai inregistrat !");
+
+      var nume = this.registerForm.controls['nume'].value;
+      localStorage.setItem('Nume',nume);
+
+      var prenume = this.registerForm.controls['prenume'].value;
+      localStorage.setItem('Prenume',prenume);
+
+      var email = this.registerForm.controls['email'].value;
+      localStorage.setItem('Email',email);
+
+      var tel= this.registerForm.controls['tel'].value;
+      localStorage.setItem('Telefon',tel);
+
+      var parola = this.registerForm.controls['parola'].value;
+      localStorage.setItem('parola',parola);
+
+      this.router.navigate(['/login']);
+    }
+
+  
   }
 
-}
